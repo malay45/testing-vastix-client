104a9123eb1718a551b081893b851903bc0c2564
parth kathiriya <parth.kathiriya@aksharsoftsolutions.com>
2025-07-31 13:25:30 +0530

    add for changes

---
+++ tests/web/Home/test-user_accesses_an_'about_us'_page_that_details_the_company's_history_and_mission..spec.ts
@@ -0,0 +1,20 @@
+import { Page, Locator, test, expect, } from '@playwright/test';
+import * as allure from "allure-js-commons";
+import '../../hooks/test.setup';
+
+
+test('test', { tag: ["@Home"] }, async ({ page }, testInfo1) => {
+
+
+    await allure.step("Navigate to https://aksharsoftsolutions.com/ : https://aksharsoftsolutions.com/", async () => { await page.goto('https://aksharsoftsolutions.com/'); });
+
+    await allure.step("Click WooCommerce : ", async () => { await page.locator('xpath=/html[1]/body[1]/div[1]/section[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[6]/a[1]/div[1]').waitFor({ state: "visible" }); await page.locator('xpath=/html[1]/body[1]/div[1]/section[2]/div[1]/div[1]/div[1]/div[2]/div[1]/div[6]/a[1]/div[1]').click(); });
+
+    await allure.step("Click  : ", async () => { await page.locator('xpath=/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/a[1]/div[1]/div[1]/img[1]').waitFor({ state: "visible" }); await page.locator('xpath=/html[1]/body[1]/section[2]/div[1]/div[1]/div[2]/a[1]/div[1]/div[1]/img[1]').click(); });
+
+    await allure.step("Click Dr. Chaitanya Buch : ", async () => { await page.locator('xpath=/html[1]/body[1]/div[1]/div[4]/div[2]/a[1]/div[1]/div[2]').waitFor({ state: "visible" }); await page.locator('xpath=/html[1]/body[1]/div[1]/div[4]/div[2]/a[1]/div[1]/div[2]').click(); });
+
+    await allure.step("Click  : ", async () => { await page.locator('xpath=/html[1]/body[1]/div[1]/div[4]/div[2]/a[1]/div[1]/div[1]/img[1]').waitFor({ state: "visible" }); await page.locator('xpath=/html[1]/body[1]/div[1]/div[4]/div[2]/a[1]/div[1]/div[1]/img[1]').click(); });
+
+
+});
