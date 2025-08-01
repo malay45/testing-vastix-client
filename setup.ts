a4c78d2181e525a55caaeca25f7fa2cfdda79011
Malay Solanki <59390136+malay45@users.noreply.github.com>
2025-08-01 13:25:42 +0530

    Update setup.ts

--- setup.ts
+++ setup.ts
@@ -1,6 +1,7 @@
 import fs from "node:fs"
 
 async function setup() {
+    console.log("Test)
     if(fs.existsSync('allure-results')){
         fs.rmdirSync('allure-results', { recursive: true});
     }
